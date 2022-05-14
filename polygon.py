2482999a6c2b3d1d4864f7d24cc36edd263eb503
clutch70 <b.custard@outlook.com>
2022-05-14 08:32:23 -0700

    calling contract functions

--- polygon.py
+++ polygon.py
@@ -5,7 +5,7 @@ version = 0.001
 
 explorerUrl = "https://polygonscan.com/"
 
-
+#Define what HTMLParser should do with data it encounters
 class polygonScanParser(HTMLParser):
     contractPageData = []
     def handle_data(self, data):
@@ -18,6 +18,7 @@ def html_to_list(rawHtml, parser):
     # pass the raw HTML to HTMLParser
     parser.feed(rawHtml)
 
+#Get these from the explorer site when clicking on "Read Contract"
 def get_url_trailer(addrtype):
     if addrtype == "address":
         return "code"
@@ -25,11 +26,13 @@ def get_url_trailer(addrtype):
         return "readContract"
     pass
 
+#Dynamically populate the contract ABI from the explorer site
 def get_abi(w3, addrType, contractAddr):
     url_trailer = get_url_trailer(addrType)
     url = explorerUrl + addrType + "/" + contractAddr + "#" + url_trailer
     r = requests.get(url=url)
     parser = polygonScanParser()
+    #Pass HTML data to HTMLParser
     html_to_list(r.text, parser)
     for i in parser.contractPageData:
         if "inputs\":[]," in i:
