ca869782fa77db17a1321e87c76f2d7103cbba3c
clutch70 <b.custard@outlook.com>
2022-05-14 08:12:02 -0700

    calling contract functions

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
 from web3 import Web3
-#from web3 import EthereumTesterProvider
+# from web3 import EthereumTesterProvider
 import polygon
-#create your own keys.py with an API token and whatever other private things you need
+# create your own keys.py with an API token and whatever other private things you need
 import keys
 
 url = "https://eth-mainnet.alchemyapi.io/v2/" + keys.urlToken
@@ -11,7 +11,9 @@ polygonWss = "wss://polygon-mainnet.g.alchemy.com/v2/" + keys.urlToken
 address = keys.ledg01Addr
 lpContract = "0xdC9232E2Df177d7a12FdFf6EcBAb114E2231198D"
 tokenContract = "0x1bfd67037b42cf73acf2047067bd4f2c47d9bfd6"
-#tokenData = [{"polygon":{"baseContract":"0x2791Bca1f2de4661ED88A30C99A7a9449Aa84174"},
+
+
+# tokenData = [{"polygon":{"baseContract":"0x2791Bca1f2de4661ED88A30C99A7a9449Aa84174"},
 #                {"lpContracts":
 #                     []
 #                 }
@@ -22,28 +24,19 @@ tokenContract = "0x1bfd67037b42cf73acf2047067bd4f2c47d9bfd6"
 
 def run():
     w3 = Web3(Web3.WebsocketProvider(polygonWss))
-    #latest = w3.eth.get_block('latest')
-    #print(latest)
+    # latest = w3.eth.get_block('latest')
+    # print(latest)
 
-    #balance = w3.fromWei(w3.eth.get_balance(address), 'ether')
-    #decBalance = w3.fromWei(balance, 'ether')
-    #print(balance,"MATIC")
-    abi = polygon.get_abi(w3,"address",lpContract)
-    contract_instance = w3.eth.contract(address = lpContract, abi = abi)
+    # balance = w3.fromWei(w3.eth.get_balance(address), 'ether')
+    # decBalance = w3.fromWei(balance, 'ether')
+    # print(balance,"MATIC")
+    abi = polygon.get_abi(w3, "address", lpContract)
+    contract_instance = w3.eth.contract(address=lpContract, abi=abi)
     print(contract_instance.all_functions())
     print(contract_instance.functions.token0().call())
 
-    contractPageData = []
-    #polygon.reset_page_data()
-    #print(polygon.get_abi(w3, "address", tokenContract))
-    contractPageData = []
-    #polygon.reset_page_data()
 
-# See PyCharm help at https://www.jetbrains.com/help/pycharm/
-
-#def get_lp_prices:
-
-
+# def get_lp_prices:
 
 
 if __name__ == '__main__':
