d7b12b09373f1e0bcbeccb72a38b6c0fe5353641
clutch70 <b.custard@outlook.com>
2022-05-14 08:22:57 -0700

    calling contract functions

--- common.py
+++ common.py
@@ -1,7 +1,13 @@
+import requests
+from html.parser import HTMLParser
+import polygon
+
 #Functions implemented in this module should handle all supported chains in main.tokenData
 
 
-
+def get_abi(w3, addrType, contractAddr, chain):
+    if chain == "polygon":
+        return polygon.get_abi(w3, addrType, contractAddr)
 
 
 def run():
--- main.py
+++ main.py
@@ -3,6 +3,7 @@ from web3 import Web3
 import polygon
 # create your own keys.py with an API token and whatever other private things you need
 import keys
+import common
 
 url = "https://eth-mainnet.alchemyapi.io/v2/" + keys.urlToken
 polygonUrl = "https://polygon-mainnet.g.alchemy.com/v2/" + keys.urlToken
@@ -30,7 +31,9 @@ def run():
     # balance = w3.fromWei(w3.eth.get_balance(address), 'ether')
     # decBalance = w3.fromWei(balance, 'ether')
     # print(balance,"MATIC")
-    abi = polygon.get_abi(w3, "address", lpContract)
+    #abi = polygon.get_abi(w3, "address", lpContract)
+    #eventually, the chain parameter (polygon) needs to be replaced with tokenData
+    abi = common.get_abi(w3, "address", lpContract, "polygon")
     contract_instance = w3.eth.contract(address=lpContract, abi=abi)
     print(contract_instance.all_functions())
     print(contract_instance.functions.token0().call())
