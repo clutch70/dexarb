04ed41b496b706081312b2cdba3bc00a6fb19839
clutch70 <b.custard@outlook.com>
2022-05-14 07:37:04 -0700

    calling contract functions

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 from web3 import Web3
 #from web3 import EthereumTesterProvider
 import polygon
+#create your own keys.py with an API token and whatever other private things you need
 import keys
 
 # This is a sample Python script.
@@ -13,13 +14,9 @@ url = "https://eth-mainnet.alchemyapi.io/v2/" + keys.urlToken
 polygonUrl = "https://polygon-mainnet.g.alchemy.com/v2/" + keys.urlToken
 wss = "wss://eth-mainnet.alchemyapi.io/v2/" + keys.urlToken
 polygonWss = "wss://polygon-mainnet.g.alchemy.com/v2/" + keys.urlToken
-address = "0x0F2e54b53570AD0f89F9AC2C697d846810F394D7"
+address = keys.ledg01Addr
 lpContract = "0xdC9232E2Df177d7a12FdFf6EcBAb114E2231198D"
 tokenContract = "0x1bfd67037b42cf73acf2047067bd4f2c47d9bfd6"
-
-#def get_lp_prices:
-
-
 
 def run():
     w3 = Web3(Web3.WebsocketProvider(polygonWss))
@@ -29,14 +26,23 @@ def run():
     #balance = w3.fromWei(w3.eth.get_balance(address), 'ether')
     #decBalance = w3.fromWei(balance, 'ether')
     #print(balance,"MATIC")
-    print(polygon.get_abi(w3,"address",lpContract))
+    abi = polygon.get_abi(w3,"address",lpContract)
+    contract_instance = w3.eth.contract(address = lpContract, abi = abi)
+    print(contract_instance.all_functions())
+    print(contract_instance.functions.token0().call())
+
     contractPageData = []
     #polygon.reset_page_data()
-    print(polygon.get_abi(w3, "address", tokenContract))
+    #print(polygon.get_abi(w3, "address", tokenContract))
     contractPageData = []
     #polygon.reset_page_data()
+
+# See PyCharm help at https://www.jetbrains.com/help/pycharm/
+
+#def get_lp_prices:
+
+
+
 
 if __name__ == '__main__':
     run()
-
-# See PyCharm help at https://www.jetbrains.com/help/pycharm/
